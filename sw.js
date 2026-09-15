8b85ad7dc2bede5d89586863050056bdcd5b4036
Wesley Eenjes <P7uisdev@gmail.com>
2026-09-15 19:41:56 +0200

    Fix camera restart and recording chunks

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const MC_CACHE_VERSION = 'media-creation-tool-v1.2.0';
+const MC_CACHE_VERSION = 'media-creation-tool-v1.2.1';
 const MC_INDEX_URL = new URL('./index.html', self.registration.scope).toString();
 const MC_APP_SHELL = [
   new URL('./', self.registration.scope).toString(),
